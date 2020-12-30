2e3814ff4a576eaeb6563981315563d368247fa8
rajjadhav-hash <76695879+rajjadhav-hash@users.noreply.github.com>
2020-12-30 20:30:26 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,86 @@
+
+var monkey , monkey_running
+var banana ,bananaImage, obstacle, obstacleImage
+var bananaGroup, obstacleGroup
+var score
+var survivalTime=0;
+function preload(){
+  
+  
+  monkey_running =            loadAnimation("sprite_0.png","sprite_1.png","sprite_2.png","sprite_3.png","sprite_4.png","sprite_5.png","sprite_6.png","sprite_7.png","sprite_8.png")
+  
+  bananaImage = loadImage("banana.png");
+  obstacleImage = loadImage("obstacle.png");
+ 
+}
+
+
+
+function setup() {
+  monkey=createSprite(80,315,20,20);
+  monkey.addAnimation("moving",monkey_running);
+  monkey.scale=0.1;
+  
+  ground=createSprite(400,350,900,10);
+  ground.velocityX=-4;
+  ground.x=ground.width/2;
+  console.log(ground.x);
+  
+  bananaGroup=createGroup();
+  obstacleGroup=createGroup();
+  
+  
+}
+
+
+function draw() {
+  background(255);
+  text("Survival Time : "+survivalTime,100,50);
+  stroke("black");
+  textSize(20);
+  fill("black");
+  survivalTime=Math.round(frameCount/frameRate())
+  
+  if (ground.x < 0){
+      ground.x = ground.width/2;
+    }
+  monkey.collide(ground);
+    if(keyDown("space")&& monkey.y >=0){
+     monkey.velocityY=-13;
+       }
+    monkey.velocityY = monkey.velocityY + 0.8;
+  
+    spawnBananas();
+    spawnObstacles();
+  
+  
+  
+drawSprites();
+  
+  
+  
+}
+
+  function spawnBananas(){
+    if(frameCount%80===0){
+      banana=createSprite(600,50,10,10);
+      banana.y=Math.round(random(120,200));
+      banana.addImage(bananaImage);
+      banana.scale=0.1;
+      banana.velocityX=-3;
+      banana.lifetime=175;
+      bananaGroup.add(banana);
+    }
+  }
+  function spawnObstacles(){
+    if (frameCount % 300 === 0) {
+    var obstacle = createSprite(500, 365, 23, 32);
+    obstacle.velocityX = -3;
+    obstacle.addImage("obstacle", obstacleImage);
+    obstacle.scale = 0.2;
+    obstacleGroup.add(obstacle);
+    obstacleGroup.setLifetimeEach=100;
+    
+  }
+    
+  }
